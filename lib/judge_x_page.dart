ea73d94dd835db75731f11ea038eca44b5cc0285
k-0107 <k.tkm0107@gmail.com>
2024-12-05 19:46:05 +0900

    言葉づかいで年齢、性別を判定

---
+++ lib/judge_x_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class JudgeXPage extends HookWidget {
+  const JudgeXPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("TEST"),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gemini/judge_x_page.dart';
 
-void main() async {
-  final model = GenerativeModel(
-    model: 'gemini-pro',
-    apiKey: apiKey,
+void main() {
+  const app = ProviderScope(
+    child: MaterialApp(
+      home: JudgeXPage(),
+    ),
   );
-  const prompt = '子犬の名前を5匹分考えてください';
-  final content = [
-    Content.text(prompt),
-  ];
 
-  final response = await model.generateContent(content);
-
-  debugPrint(response.text);
+  runApp(app);
 }
